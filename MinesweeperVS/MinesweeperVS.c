f674dc320b7c59a08aeb67ee15482a34718c56f7
RustyyLuma <86574157+Rustyymp4@users.noreply.github.com>
2023-03-09 19:50:41 +0100

    Add files via upload

--- MinesweeperVS/MinesweeperVS.c
+++ MinesweeperVS/MinesweeperVS.c
@@ -16,7 +16,8 @@
 int i, j;
 int winStatus = 0;
 int loseStatus = 0;
-int inputI, inputJ;
+int inputI = 17;
+int inputJ = 17;
 int checkI, checkJ;
 int flags = 0;
 
@@ -29,25 +30,13 @@ int flags = 0;
 #define COLOR_RESET "\x1b[0m"
 
 
+
+
 #define wSize 16
 #define hSize 16
 #define mines 35
 
 int remaining = hSize * wSize - mines;
-
-const char* colors[9] =
-{
-	"[#]",
-	BLEU "[1]" COLOR_RESET,
-	VERT "[2]" COLOR_RESET,
-	ROUGE "[3]" COLOR_RESET,
-	MAGENTA "[4]" COLOR_RESET,
-	JAUNE "[5]" COLOR_RESET,
-	JAUNE "[6]" COLOR_RESET,
-	JAUNE "[7]" COLOR_RESET,
-	JAUNE "[8]" COLOR_RESET
-
-};
 
 const char* l[wSize] = { " 0]"," 1]"," 2]"," 3]"," 4]"," 5]"," 6]"," 7]"," 8]"," 9]","10]","11]","12]","13]","14]","15]" };
 
@@ -61,14 +50,15 @@ typedef struct Cell {
 
 
 
+
 //i == ligne
 //j == collone
 
-void check(int inputI, int inputJ, struct Cell grid[16][16]) {
+void check(int inputI, int inputJ, struct Cell grid[16][16], SDL_Texture* colors[9]) {
 
 	///On creuse les cases d'a cote
 
-	grid[inputI][inputJ].display = colors[grid[inputI][inputJ].nearby];
+ 	grid[inputI][inputJ].display = colors[grid[inputI][inputJ].nearby];
 
 
 	if (grid[inputI][inputJ].bomb == 1) {
@@ -83,48 +73,49 @@ void check(int inputI, int inputJ, struct Cell grid[16][16]) {
 	}
 
 	if (wSize >= inputI && inputI > 0 && grid[inputI - 1][inputJ].isSelected == 0) {
-		check(inputI - 1, inputJ, grid);
+		check(inputI - 1, inputJ, grid, colors);
 		if (hSize >= inputJ && inputJ > 0 && grid[inputI - 1][inputJ - 1].isSelected == 0) {
 			grid[inputI][inputJ].isSelected = 1;
 			remaining--;
-			check(inputI - 1, inputJ - 1, grid);
+			check(inputI - 1, inputJ - 1, grid, colors);
 		}
 		if (0 <= inputJ && inputJ < hSize - 1 && grid[inputI - 1][inputJ + 1].isSelected == 0) {
 			grid[inputI][inputJ].isSelected = 1;
 			remaining--;
-			check(inputI - 1, inputJ + 1, grid);
+			check(inputI - 1, inputJ + 1, grid, colors);
 		}
 	}
 
 	if (hSize >= inputJ && inputJ > 0 && grid[inputI][inputJ - 1].isSelected == 0) {
 		grid[inputI][inputJ].isSelected = 1;
 		remaining--;
-		check(inputI, inputJ - 1, grid);
+		check(inputI, inputJ - 1, grid, colors);
 		if (inputI <= wSize - 1 && grid[inputI + 1][inputJ - 1].isSelected == 0) {
 			grid[inputI][inputJ].isSelected = 1;
 			remaining--;
-			check(inputI + 1, inputJ - 1, grid);
+			check(inputI + 1, inputJ - 1, grid, colors);
 		}
 	}
 
 	if (0 <= inputI && inputI < wSize - 1 && grid[inputI + 1][inputJ].isSelected == 0) {
 		grid[inputI][inputJ].isSelected = 1;
 		remaining--;
-		check(inputI + 1, inputJ, grid);
+		check(inputI + 1, inputJ, grid, colors);
 		if (0 <= inputJ && inputJ < hSize - 1 && grid[inputI + 1][inputJ + 1].isSelected == 0) {
 			grid[inputI][inputJ].isSelected = 1;
 			remaining--;
-			check(inputI + 1, inputJ + 1, grid);
+			check(inputI + 1, inputJ + 1, grid, colors);
 		}
 	}
 
 	if (0 <= inputJ && inputJ < hSize - 1 && grid[inputI][inputJ + 1].isSelected == 0) {
 		grid[inputI][inputJ].isSelected = 1;
 		remaining--;
-		check(inputI, inputJ + 1, grid);
+		check(inputI, inputJ + 1, grid,colors);
 	}
 
 }
+
 
 
 /// <gridDIsplayInit>
@@ -136,51 +127,138 @@ void check(int inputI, int inputJ, struct Cell grid[16][16]) {
 /// </gridDIsplayInit>
 
 
-
 void gaming() {
 
 	char choixRF = "";
 	Cell grid[wSize][hSize];
 
-///Initalisation du display 
 
-	SDL_Init(SDL_INIT_VIDEO);
+
+	///Initalisation du display 
+
+	int width = 800;
+	int height = 800;
+	int w_case = width / wSize;
+	int h_case = height / hSize;
+
 	SDL_Window* window = SDL_CreateWindow("Display", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 100, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_Surface* image = NULL;
-	SDL_FreeSurface(image);
-	image = SDL_LoadBMP("Bomb.BMP");
-	SDL_Texture* monImage = SDL_CreateTextureFromSurface(renderer, image);
-	SDL_RenderPresent(renderer);
 
-	SDL_Delay(3000);
+	///Initialisation des images
 
+	SDL_Surface* Tile = NULL;
+	SDL_Texture* textureTile = NULL;
+	Tile = SDL_LoadBMP("Tiles.bmp");
+	textureTile = SDL_CreateTextureFromSurface(renderer, Tile);
+	SDL_FreeSurface(Tile);
 
+	SDL_Surface* Ground = NULL;
+	SDL_Texture* textureGround = NULL;
+	Ground = SDL_LoadBMP("Ground.bmp");
+	textureGround = SDL_CreateTextureFromSurface(renderer, Ground);
+	SDL_FreeSurface(Ground);
 
-///Initialisation du jeu 
+	SDL_Surface* Bomb = NULL;
+	SDL_Texture* textureBomb = NULL;
+	Bomb = SDL_LoadBMP("Bomb.bmp");
+	textureBomb = SDL_CreateTextureFromSurface(renderer, Bomb);
+	SDL_FreeSurface(Bomb);
 
+	SDL_Surface* Flag = NULL;
+	SDL_Texture* textureFlag = NULL;
+	Flag = SDL_LoadBMP("Bomb.bmp");
+	textureFlag = SDL_CreateTextureFromSurface(renderer, Flag);
+	SDL_FreeSurface(Flag);
 
-	///Initialisation du tableau
+	SDL_Surface* Nearby1 = NULL;
+	SDL_Texture* textureNearby1 = NULL;
+	Nearby1 = SDL_LoadBMP("Nearby1.bmp");
+	textureNearby1 = SDL_CreateTextureFromSurface(renderer, Nearby1);
+	SDL_FreeSurface(Nearby1);
+
+	SDL_Surface* Nearby2 = NULL;
+	SDL_Texture* textureNearby2 = NULL;
+	Nearby2 = SDL_LoadBMP("Nearby2.bmp");
+	textureNearby2 = SDL_CreateTextureFromSurface(renderer, Nearby2);
+	SDL_FreeSurface(Nearby2);
+
+	SDL_Surface* Nearby3 = NULL;
+	SDL_Texture* textureNearby3 = NULL;
+	Nearby3 = SDL_LoadBMP("Nearby3.bmp");
+	textureNearby3 = SDL_CreateTextureFromSurface(renderer, Nearby3);
+	SDL_FreeSurface(Nearby3);
+
+	SDL_Surface* Nearby4 = NULL;
+	SDL_Texture* textureNearby4 = NULL;
+	Nearby4 = SDL_LoadBMP("Nearby4.bmp");
+	textureNearby4 = SDL_CreateTextureFromSurface(renderer, Nearby4);
+	SDL_FreeSurface(Nearby4);
+
+	SDL_Surface* Nearby5 = NULL;
+	SDL_Texture* textureNearby5 = NULL;
+	Nearby5 = SDL_LoadBMP("Nearby5.bmp");
+	textureNearby5 = SDL_CreateTextureFromSurface(renderer, Nearby5);
+	SDL_FreeSurface(Nearby5);
+
+	SDL_Surface* Nearby6 = NULL;
+	SDL_Texture* textureNearby6 = NULL;
+	Nearby6 = SDL_LoadBMP("Nearby6.bmp");
+	textureNearby6 = SDL_CreateTextureFromSurface(renderer, Nearby6);
+	SDL_FreeSurface(Nearby6);
+
+	SDL_Surface* Nearby7 = NULL;
+	SDL_Texture* textureNearby7 = NULL;
+	Nearby7 = SDL_LoadBMP("Nearby7.bmp");
+	textureNearby7 = SDL_CreateTextureFromSurface(renderer, Nearby7);
+	SDL_FreeSurface(Nearby7);
+
+	SDL_Surface* Nearby8 = NULL;
+	SDL_Texture* textureNearby8 = NULL;
+	Nearby8 = SDL_LoadBMP("Nearby7.bmp");
+	textureNearby8 = SDL_CreateTextureFromSurface(renderer, Nearby8);
+	SDL_FreeSurface(Nearby8);
+
+	SDL_Texture* colors[9] =
+	{
+		textureGround,
+		textureNearby1,
+		textureNearby2,
+		textureNearby3,
+		textureNearby4,
+		textureNearby5,
+		textureNearby6,
+		textureNearby7,
+		textureNearby8
+
+	};
+
 
 	for (i = 0; i < wSize; i++) {
 		for (j = 0; j < hSize; j++) {
-			grid[i][j].display = "[ ]";
+			int x = 0 + i * w_case;
+			int y = 0 + j * h_case;
+			grid[i][j].display = textureTile;
 			grid[i][j].bomb = 0;
 			grid[i][j].nearby = 0;
 			grid[i][j].isSelected = 0;
-			grid[i][j].isFlagged = 0;
+			grid[i][j].isFlagged = 0; 
+			SDL_Rect rect = { x, y, w_case, h_case };
+			SDL_RenderCopy(renderer, grid[i][j].display, NULL, &rect);
 		}
 	}
 
 
 
 
-	///Initialisation des bombes
+	///Initialisation du jeu 
+
+
+		///Initialisation des bombes
 
 	int i, j, count = 0;
 	while (count < mines) {
+
+
 		i = rand() % wSize;
 		j = rand() % hSize;
 		if (grid[i][j].bomb != 1) {
@@ -236,126 +314,143 @@ void gaming() {
 		}
 	}
 
-	///Print du tableau
-
-	system("cls");
-	printf(CYAN " /] 0] 1] 2] 3] 4] 5] 6] 7] 8] 9]10]11]12]13]14]15]\n" COLOR_RESET);
-
-	for (i = 0; i < wSize; i++) {
-		printf(CYAN "%s" COLOR_RESET, l[i]);
-		for (j = 0; j < hSize; j++) {
-			printf("%s", grid[i][j].display);
-		}
-		printf("\n");
-	}
+	///Boucle de jeu
 
 
-	///Defaite
+	while (winStatus == 0 && loseStatus == 0) {
 
-	if (grid[inputI][inputJ].bomb == 1) {
-		system("cls");
-		printf(CYAN " /] 0] 1] 2] 3] 4] 5] 6] 7] 8] 9]10]11]12]13]14]15]\n" COLOR_RESET);
-
-		//on print le tableau quand on est mort
-		for (i = 0; i < wSize; i++) {
-			printf(CYAN "%s" COLOR_RESET, l[i]);
-			for (j = 0; j < hSize; j++) {
-
-
-				if (grid[i][j].bomb == 1) {
-					grid[i][j].display = ROUGE "[*]" COLOR_RESET;
-				}
-
-				grid[inputI][inputJ].display = ROUGE "[X]" COLOR_RESET;
-
-				if (grid[i][j].bomb == 0 && grid[i][j].isFlagged == 1) {
-					grid[i][j].display = JAUNE "[X]" COLOR_RESET;
-				}
-
-				printf("%s", grid[i][j].display);
-			}
-			printf("\n");
-		}
-		printf("Bravo tu as perdu! Gros bebe");
-		loseStatus = 1;
-		abort;
-
-	}
-
-	///Condition de victoire 
-
-	if (remaining < 0) {
-		winStatus = 1;
-		system("cls");
-		printf(CYAN " /] 0] 1] 2] 3] 4] 5] 6] 7] 8] 9]10]11]12]13]14]15]\n" COLOR_RESET);
+		choixRF = "";
 
 		for (i = 0; i < wSize; i++) {
-			printf(CYAN "%s" COLOR_RESET, l[i]);
 			for (j = 0; j < hSize; j++) {
-
-				grid[i][j].display = colors[grid[i][j].nearby];
-
-				if (grid[i][j].bomb == 1) {
-					grid[i][j].display = ROUGE "[*]" COLOR_RESET;
-				}
-
-				if (grid[i][j].bomb == 0 && grid[i][j].isFlagged == 1) {
-					grid[i][j].display = JAUNE "[X]" COLOR_RESET;
-				}
-				printf("%s", grid[i][j].display);
+				int x = 0 + i * w_case;
+				int y = 0 + j * h_case;
+				SDL_Rect rect = { x, y, w_case, h_case };
+				SDL_RenderCopy(renderer, grid[i][j].display, NULL, &rect);
 			}
-			printf("\n");
 		}
-		printf(JAUNE "Bravo, tu as gagne!" COLOR_RESET);
-		winStatus = 1;
-		abort;
+
+		SDL_RenderPresent(renderer);
+
+
+		//EVENT
+		SDL_Event event;
+		SDL_WaitEvent(&event);
+
+		if (event.type == SDL_MOUSEBUTTONDOWN) {
+			if (event.button.button == SDL_BUTTON_LEFT) {
+				inputI = event.button.x;
+				inputJ = event.button.y;
+				inputJ = inputJ / h_case;
+				inputI = inputI / w_case;
+				choixRF = 'r';
+			}
+			else if (event.button.button == SDL_BUTTON_RIGHT) {
+				inputI = event.button.x;
+				inputJ = event.button.y;
+				inputJ = inputJ / h_case;
+				inputI = inputI / w_case;
+				choixRF = 'f';
+			}
+		}
+
+		///On lance la fonction de check
+
+
+
+
+		///On met le numero et tout
+
+		if (choixRF == 'r' && grid[inputI][inputJ].isFlagged == 0 && grid[inputI][inputJ].nearby == 0) {
+			check(inputI, inputJ + 1, grid, colors);
+		}
+
+		if (choixRF == 'r' && grid[inputI][inputJ].isFlagged == 0 && grid[inputI][inputJ].nearby != 0) {
+				grid[inputI][inputJ].display = colors[grid[inputI][inputJ].nearby];
+		}
+
+
+		///On empeche de selectionner des cases avec un drappeau
+
+		if (choixRF == 'r' && grid[inputI][inputJ].isFlagged == 1) {
+
+		}
+
+		///On mets le drappeau a la case selectionnee
+
+		if (choixRF == 'f' && grid[inputI][inputJ].isFlagged == 0) {
+			grid[inputI][inputJ].isFlagged = 1;
+			flags++;
+			grid[inputI][inputJ].display = textureFlag;
+			choixRF = ' ';
+		}
+
+		///On enleve le drappeau de la case selectionnee
+
+		if (choixRF == 'f' && grid[inputI][inputJ].isFlagged == 1) {
+			grid[inputI][inputJ].isFlagged = 0;
+			grid[inputI][inputJ].display = textureTile;
+			flags--;
+			printf("Le drappeau a bien ete enleve\n");
+		}
+
+		///Defaite
+
+		if (grid[inputI][inputJ].bomb == 1 && choixRF == 'r') {
+				for (i = 0; i < wSize; i++) {
+					for (j = 0; j < hSize; j++) {
+
+
+						if (grid[i][j].bomb == 1) {
+							grid[i][j].display = textureBomb;
+						}
+
+						if (grid[i][j].bomb == 0 && grid[i][j].isFlagged == 1) {
+							grid[i][j].display = textureBomb;
+						}
+
+						int x = 0 + i * w_case;
+						int y = 0 + j * h_case;
+						SDL_Rect rect = { x, y, w_case, h_case };
+						SDL_RenderCopy(renderer, grid[i][j].display, NULL, &rect);
+						abort;
+					}
+				}
+		}
+
+		///Condition de victoire 
+
+		if (remaining < 0) {
+			
+			for (i = 0; i < wSize; i++) {
+				for (j = 0; j < hSize; j++) {
+
+					grid[i][j].display = colors[grid[i][j].nearby];
+
+					if (grid[i][j].bomb == 1) {
+						grid[i][j].display = textureBomb;
+					}
+
+					if (grid[i][j].bomb == 0 && grid[i][j].isFlagged == 1) {
+						grid[i][j].display = textureBomb;
+					}
+					int x = 0 + i * w_case;
+					int y = 0 + j * h_case;
+					SDL_Rect rect = { x, y, w_case, h_case };
+					SDL_RenderCopy(renderer, grid[i][j].display, NULL, &rect);
+					printf("on imprime le tableau gagner");
+					abort;
+				}
+			}
+			
+		}
 
 	}
 
 
-	///On lance la fonction de check
-
-	/*if ((choixRF == 'r') || (choixRF == 'R') && grid[inputI][inputJ].isFlagged == 0 && grid[inputI][inputJ].nearby == 0) {
-		check(inputI, inputJ, grid);
-	}*/
-
-
-	///On met le numero et tout
-
-	/*if ((choixRF == 'r') || (choixRF == 'R') && grid[inputI][inputJ].isFlagged == 0 && grid[inputI][inputJ].nearby != 0) {
-		grid[inputI][inputJ].display = colors[grid[inputI][inputJ].nearby];
-	}*/
-
-
-	///On empeche de selectionner des cases avec un drappeau
-
-	/*if ((choixRF == 'r') || (choixRF == 'R') && grid[inputI][inputJ].isFlagged == 1) {
-
-		break;
-	}*/
-
-	///On mets le drappeau a la case selectionnee
-
-	/*if ((choixRF == 'f') || (choixRF == 'F') && grid[inputI][inputJ].isFlagged == 0) {
-		grid[inputI][inputJ].isFlagged = 1;
-		flags++;
-		grid[inputI][inputJ].display = JAUNE "[>]" COLOR_RESET;
-
-		break;
-	}*/
-
-	///On enleve le drappeau de la case selectionnee
-
-	/*if (((choixRF == 'f') || (choixRF == 'F')) && grid[inputI][inputJ].isFlagged == 1) {
-		grid[inputI][inputJ].isFlagged = 0;
-		grid[inputI][inputJ].display = "[ ]";
-		flags--;
-		printf("Le drappeau a bien ete enleve\n");
-
-		break;
-	}*/
 
 }
+
 
 
 /// <main>
@@ -378,23 +473,17 @@ int main() {
 	srand(time(NULL));
 
 	//Execution
-	
-	while (winStatus == 0 && loseStatus == 0){
+	while(winStatus == 0 && loseStatus == 0 ){
 		gaming();
 		srand(time(NULL));
-		return 0;
+		
 	}
 
+	
 
-	if (winStatus == 1) {
-		return 0;
-	}
-
-	if (loseStatus == 1) {
-		return 0;
-	}
+	
 
 	//Destruction
-
+	SDL_Quit();
 
 }
